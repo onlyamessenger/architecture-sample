4c0b2396fdf18c0cf4e8b4c7261352fc77dc97ed
Christian Wheeler <christian.wheeler@epiuse.com>
2019-09-19 22:11:20 +0200

    Add Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-
+    apiKey: 'AIzaSyCj-y6EJ_zG7kWmZRmub0L9WlEpyL2_xYA',
+    authDomain: 'architecture-cc819.firebaseapp.com',
+    databaseURL: 'https://architecture-cc819.firebaseio.com',
+    projectId: 'architecture-cc819',
+    storageBucket: 'architecture-cc819.appspot.com',
+    messagingSenderId: '427109572242',
+    appId: '1:427109572242:web:41f7bea707904dfa16104b',
   },
 }
